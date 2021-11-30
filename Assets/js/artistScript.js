7921adb080e5f652bca3aa655486d07a1be93f21
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2021-11-29 23:51:11 -0600

    made elements hidden until the user searches on event page

--- Assets/js/artistScript.js
+++ Assets/js/artistScript.js
@@ -4,129 +4,155 @@ let upcomingEvents = document.querySelector("h4");
 let eventSection = document.querySelector("#event-section");
 let artistHeader = document.querySelector("h2");
 let artistPicture = document.querySelector("#artist-picture");
-let artistLink = document.querySelector("#artist-link")
+let artistLink = document.querySelector("#artist-link");
+let searchBtn = document.querySelector(".btn");
+let searchInput = document.querySelector(".search");
+let artistPicture = document.querySelector("#artist-picture");
 
 
-fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=lady-gaga', {
+
+function clearCards() {
+    while (eventSection.firstChild) {
+        eventSection.removeChild(eventSection.firstChild);
+    }
+};
+
+function getArtist(event) {
+    event.preventDefault();
+    clearCards();
+    artistPicture.setAttribute("visibility", "visible")
+    let search = searchInput.value;
+    search = search.toLowerCase();
+    search = search.replace(" ", "-");
+    console.log(search);
+    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${search}`, {
         "method": "GET",
         "headers": {
-              "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-              "x-rapidapi-key": "b527f11ed1mshff817c77ab9f92cp1d9953jsnd565aa730026"
-          }
-  })
-  .then(response => {
-      return response.json()
-  })
-  .then(data => {
-      console.log(data)
-      let artistID = data.data[0].artist.id
-        fetch(`https://deezerdevs-deezer.p.rapidapi.com/artist/${artistID}`, {
-            "method": "GET",
-            "headers": {
-                 "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-                 "x-rapidapi-key": "b527f11ed1mshff817c77ab9f92cp1d9953jsnd565aa730026"
-          }
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+            "x-rapidapi-key": "b527f11ed1mshff817c77ab9f92cp1d9953jsnd565aa730026"
+        }
     })
     .then(response => {
         return response.json()
     })
-    .then(artistData => {
-        console.log(artistData)
-        artistHeader.textContent = artistData.name
-        artistPicture.setAttribute("src", artistData.picture_medium)
-        artistLink.setAttribute("href", artistData.link)
-        
-    });
-  });
-
-fetch(`https://api.seatgeek.com/2/events?performers.slug=lady-gaga&client_id=MTk3NzQyNzh8MTYzODE5OTQ5Mi4zNzY5OTA2`, {
-	"method": "GET",
-	"headers": {
-	}
-})
-.then(response => {
-    return response.json()
-})
-.then(data => {
-	console.log(data);
-    if (data.events === null) {
-        upcomingEvents.textContent = "There are no upcoming events";
-    } else {
-        for (let i = 0; i < 3; i++) {
-            if (data.events[i]) {
-                let concertDate = data.events[i].datetime_local.substring(0,4)
-                concertDate = parseInt(concertDate)
-                console.log(concertDate)
-                if (concertDate < 2023) {
-                    let card = document.createElement("div")
-                    card.setAttribute("class", "card");
-                    // card.setAttribute("style",  "width:18rem")
-                    eventSection.appendChild(card);
-
-                    let concert = document.createElement("div")
-                    concert.setAttribute("class", "card-body");
-                    card.appendChild(concert);
-
-                    let cardTitle = document.createElement("h5")
-                    cardTitle.setAttribute("class", "card-title");
-                    cardTitle.textContent = data.events[i].title
-                    concert.append(cardTitle);
-
-                    let cardTime = document.createElement("h5")
-                    cardTime.setAttribute("class", "card-subtitle mb-2 text-muted");
-                    cardTime.textContent = data.events[i].datetime_local
-                    concert.append(cardTime);
-
-                    let cardText = document.createElement("p")
-                    cardText.setAttribute("class", "card-text");
-                    cardText.textContent = data.events[i].venue.name + " " + data.events[i].venue.display_location
-                    concert.append(cardText);
-
-                    let cardUrl = document.createElement("a")
-                    cardUrl.setAttribute("href", data.events[i].url) 
-                    cardUrl.setAttribute("target", "_blank")
-                    cardUrl.textContent = "Buy Tickets"
-                    concert.append(cardUrl);
-                } else {
-
-                    let card = document.createElement("div")
-                    card.setAttribute("class", "card");
-                    // card.setAttribute("style",  "width:18rem")
-                    eventSection.appendChild(card);
-
-                    let concert = document.createElement("div")
-                    concert.setAttribute("class", "card-body");
-                    card.appendChild(concert);
-
-                    let cardTitle = document.createElement("h5")
-                    cardTitle.setAttribute("class", "card-title");
-                    cardTitle.textContent = data.events[i].title
-                    concert.append(cardTitle);
-
-                    let cardTime = document.createElement("h5")
-                    cardTime.setAttribute("class", "card-subtitle mb-2 text-muted");
-                    cardTime.textContent = "TBD"
-                    concert.append(cardTime);
-
-                    let cardText = document.createElement("p")
-                    cardText.setAttribute("class", "card-text");
-                    cardText.textContent = data.events[i].venue.name + " " + data.events[i].venue.display_location
-                    concert.append(cardText);
-
-                    let cardUrl = document.createElement("a")
-                    cardUrl.setAttribute("href", data.events[i].url) 
-                    cardUrl.setAttribute("target", "_blank")
-                    cardUrl.textContent = "Buy Tickets"
-                    concert.append(cardUrl);
+    .then(data => {
+        console.log(data)
+        let artistID = data.data[0].artist.id
+        fetch(`https://deezerdevs-deezer.p.rapidapi.com/artist/${artistID}`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+                    "x-rapidapi-key": "b527f11ed1mshff817c77ab9f92cp1d9953jsnd565aa730026"
                 }
-            } else {
-                return;
+            })
+            .then(response => {
+                return response.json()
+            })
+            .then(artistData => {
+                console.log(artistData)
+                artistHeader.textContent = artistData.name
+                artistPicture.setAttribute("src", artistData.picture_medium)
+                artistLink.setAttribute("href", artistData.link)
+                
+            });
+    });
+    
+    fetch(`https://api.seatgeek.com/2/events?performers.slug=${search}&client_id=MTk3NzQyNzh8MTYzODE5OTQ5Mi4zNzY5OTA2`, {
+        "method": "GET",
+        "headers": {
+        }
+    })
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        console.log(data);
+        if (data.events.length === 0) {
+            upcomingEvents.textContent = "There are no upcoming events";
+            artistLink.textContent = "Artist's Tracklist";
+        } else {
+            for (let i = 0; i < 3; i++) {
+                if (data.events[i]) {
+                    let concertDate = data.events[i].datetime_local.substring(0,4)
+                    concertDate = parseInt(concertDate)
+                    console.log(concertDate)
+                    if (concertDate < 2023) {
+                        let card = document.createElement("div")
+                        card.setAttribute("class", "card");
+                        // card.setAttribute("style",  "width:18rem")
+                        eventSection.appendChild(card);
+                        
+                        let concert = document.createElement("div")
+                        concert.setAttribute("class", "card-body");
+                        card.appendChild(concert);
+                        
+                        let cardTitle = document.createElement("h5")
+                        cardTitle.setAttribute("class", "card-title");
+                        cardTitle.textContent = data.events[i].title
+                        concert.append(cardTitle);
+                        
+                        let cardTime = document.createElement("h5")
+                        cardTime.setAttribute("class", "card-subtitle mb-2 text-muted");
+                        cardTime.textContent = data.events[i].datetime_local
+                        concert.append(cardTime);
+                        
+                        let cardText = document.createElement("p")
+                        cardText.setAttribute("class", "card-text");
+                        cardText.textContent = data.events[i].venue.name + " " + data.events[i].venue.display_location
+                        concert.append(cardText);
+                        
+                        let cardUrl = document.createElement("a")
+                        cardUrl.setAttribute("href", data.events[i].url) 
+                        cardUrl.setAttribute("target", "_blank")
+                        cardUrl.textContent = "Buy Tickets"
+                        concert.append(cardUrl);
+                        
+                        upcomingEvents.textContent = "Upcoming Events"
+                        artistLink.textContent = "Artist's Tracklist"
+                    } else {
+                        
+                        let card = document.createElement("div")
+                        card.setAttribute("class", "card");
+                        // card.setAttribute("style",  "width:18rem")
+                        eventSection.appendChild(card);
+                        
+                        let concert = document.createElement("div")
+                        concert.setAttribute("class", "card-body");
+                        card.appendChild(concert);
+                        
+                        let cardTitle = document.createElement("h5")
+                        cardTitle.setAttribute("class", "card-title");
+                        cardTitle.textContent = data.events[i].title
+                        concert.append(cardTitle);
+                        
+                        let cardTime = document.createElement("h5")
+                        cardTime.setAttribute("class", "card-subtitle mb-2 text-muted");
+                        cardTime.textContent = "TBD"
+                        concert.append(cardTime);
+                        
+                        let cardText = document.createElement("p")
+                        cardText.setAttribute("class", "card-text");
+                        cardText.textContent = data.events[i].venue.name + " " + data.events[i].venue.display_location
+                        concert.append(cardText);
+                        
+                        let cardUrl = document.createElement("a")
+                        cardUrl.setAttribute("href", data.events[i].url) 
+                        cardUrl.setAttribute("target", "_blank")
+                        cardUrl.textContent = "Buy Tickets"
+                        concert.append(cardUrl);
+                        
+                        upcomingEvents.textContent = "Upcoming Events"
+                        artistLink.textContent = "Artist's Tracklist"
+                    }
+                } else {
+                    return;
+                }
             }
         }
-    }
-})
-.catch(err => {
-	console.error(err);
-});
+    })
+    .catch(err => {
+        console.error(err);
+    });
+};
 
-
+searchBtn.addEventListener("click", getArtist);
